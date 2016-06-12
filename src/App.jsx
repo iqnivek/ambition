524733960260c71a927f0881eb1851ebc8f09da9
Kevin Qi <iqnivek@gmail.com>
2016-06-12 01:20:20 -0400

    Render week

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import CalendarHeatmap from 'react-calendar-heatmap';
+import range from 'lodash.range';
 
 function shiftDate(date, numDays) {
   const newDate = new Date(date);
@@ -7,6 +8,7 @@ function shiftDate(date, numDays) {
   return newDate;
 }
 
+const DAYS = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 const MONTHS = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
 class App extends React.Component {
@@ -22,20 +24,28 @@ class App extends React.Component {
     ));
   }
 
+  renderWeek() {
+    return range(7).map((index) => (
+      <div className="day">{DAYS[index]}</div>
+    ));
+  }
+
   render() {
     return (
       <div>
         <div className="container">
-          <div className="row">
+          <div className="row m-y-3">
             <div className="col-xs-12 col-sm-6 offset-sm-3">
               <h1 className="text-xs-center">Ambition</h1>
-              <p className="text-xs-center">Make everything turquoise.</p>
+              <p className="text-xs-center">Paint everything turquoise.</p>
 
               <div className="row">
                 {this.renderMonths()}
               </div>
 
-              <div>Sun Mon Tue Wed Thu Fri Sat</div>
+              <div>
+                {this.renderWeek()}
+              </div>
             </div>
           </div>
         </div>
