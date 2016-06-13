8f8f17df3f77a8b363cbf13b82790b3fd50ab517
Kevin Qi <iqnivek@gmail.com>
2016-06-12 22:13:55 -0400

    Add state shape

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import CalendarHeatmap from 'react-calendar-heatmap';
 import CircularProgressbar from 'react-circular-progressbar';
-import range from 'lodash.range';
+import _ from 'lodash';
 
 function shiftDate(date, numDays) {
   const newDate = new Date(date);
@@ -17,6 +17,20 @@ const currentMonth = today.getMonth();
 const currentDay = today.getDay();
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      goals: [
+        { id: 1, name: 'wake up early' },
+        { id: 2, name: 'go to gym' },
+      ],
+      completedGoals: {
+        2: true,
+      },
+    };
+  }
+
   renderMonths() {
     return [0, -1, -2].map((index) => (
       <div key={index} className="col-xs-4">
@@ -28,7 +42,7 @@ class App extends React.Component {
 
   renderWeek() {
     const size = `${100 / 7.0}%`;
-    return range(7).map((index) => {
+    return _.range(7).map((index) => {
       return (
         <div key={index} className={index === currentDay ? 'day active' : 'day'} style={{ width: size }}>
           <CircularProgressbar percentage={0} textForPercentage={(percentage) => DAYS[index]} />
@@ -40,10 +54,11 @@ class App extends React.Component {
   renderGoals() {
     return (
       <ul className="goals list-unstyled">
-        <li className="active">wake up early</li>
-        <li className="active">go to gym</li>
-        <li>eat less junk food</li>
-        <li>practice piano</li>
+        {
+          this.state.goals.map((goal) => (
+            <li key={goal.id} className={this.state.completedGoals[goal.id] ? null : 'active'}>{goal.name}</li>
+          ))
+        }
       </ul>
     );
   }
@@ -55,7 +70,7 @@ class App extends React.Component {
           <div className="row m-y-3">
             <div className="col-xs-12 col-sm-6 offset-sm-3">
               <h1 className="text-xs-center">Ambition</h1>
-              <p className="text-xs-center">Paint everything turquoise.</p>
+              <p className="text-xs-center">Paint it turquoise.</p>
 
               <div className="row m-t-3">
                 {this.renderMonths()}
@@ -67,6 +82,10 @@ class App extends React.Component {
 
               <div className="m-t-3">
                 {this.renderGoals()}
+
+                <div className="text-xs-center">
+                  <button className="btn btn-lg btn-outline-primary">Add goal</button>
+                </div>
               </div>
             </div>
           </div>
