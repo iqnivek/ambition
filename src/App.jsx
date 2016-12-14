a5796d492dafbbdafa3b683722b80fd1808e7f80
Kevin Qi <iqnivek@gmail.com>
2016-12-13 22:52:51 -0800

    Extract history

--- src/App.jsx
+++ src/App.jsx
@@ -2,49 +2,10 @@ import React from 'react';
 import _ from 'lodash';
 import axios from 'axios';
 import classNames from 'classnames';
-import CalendarHeatmap from 'react-calendar-heatmap';
 import update from 'immutability-helper';
-
-function shiftDate(date, numDays) {
-  const newDate = new Date(date);
-  newDate.setDate(newDate.getDate() + numDays);
-  return newDate;
-}
-
-function lastDayOfMonth(year, month) {
-  return new Date(year, month + 1, 0);
-}
-
-function numDaysInMonth(year, month) {
-  return new Date(year, month, 0).getDate();
-}
-
-function calendarClassForValue(value) {
-  if (!value) {
-    return 'color-gitlab-0';
-  }
-  if (value.completion <= 0) {
-    return 'color-gitlab-0';
-  } else if (value.completion < 25) {
-    return 'color-gitlab-1';
-  } else if (value.completion < 50) {
-    return 'color-gitlab-2';
-  } else if (value.completion < 75) {
-    return 'color-gitlab-3';
-  } else {
-    return 'color-gitlab-4';
-  }
-}
-
-const DAYS = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
-const MONTHS = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+import CompletionHistory from './CompletionHistory';
 
 const now = new Date();
-const today = new Date(now.getFullYear(), now.getMonth(), now.getDate());
-const currentYear = today.getFullYear();
-const currentMonth = today.getMonth();
-const currentDate = today.getDate();
-const currentDay = today.getDay();
 
 const initialState = {
   goals: [],
@@ -137,28 +98,6 @@ class App extends React.Component {
     return (latestCompletions.length > 0) && latestCompletions[0].complete;
   }
 
-  renderMonths() {
-    return [-3, -2, -1, 0].map((index) => {
-      const endDate = lastDayOfMonth(currentYear, currentMonth + index);
-      const numDays = numDaysInMonth(currentMonth, currentMonth + index);
-      return (
-        <div key={index} className="month">
-          <p className="text-muted text-xs-center">{MONTHS[endDate.getMonth()]}</p>
-          <CalendarHeatmap
-            endDate={endDate}
-            numDays={numDays}
-            horizontal={false}
-            showMonthLabels={false}
-            values={[
-              { date: today, completion: 100 }
-            ]}
-            classForValue={calendarClassForValue}
-          />
-        </div>
-      );
-    });
-  }
-
   renderNewGoal() {
     return (
       <li className="active">
@@ -233,13 +172,10 @@ class App extends React.Component {
             <div className="col-xs-12 col-sm-8 offset-sm-2 col-md-6 offset-md-3">
               <h1 className="text-xs-center m-b-3">ambition</h1>
 
-              <div className="months">
-                {this.renderMonths()}
-              </div>
+              <CompletionHistory />
 
               <div className="m-t-3">
                 {this.renderGoals()}
-
                 {this.renderAddGoal()}
               </div>
             </div>
